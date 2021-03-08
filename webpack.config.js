c811b0b4f7b2017613f8ad9ed0eec7c28f691b14
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-08 14:25:09 -0300

    Ajusta build para es5

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,13 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ],
+  },
 };
