bfcfe1e90f625e3b4fd520b604391c30fc80f4b2
Gabriel Gomes <gabrielrafael2508@hotmail.com>
2021-03-12 08:51:28 -0300

    Ajusta export de initRoutes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import 'jquery';
 
-export { default as redirect } from './redirect';
-export { default as argument } from './argument';
-export { default as Controller } from './Controller';
-export { default as controllerRegister, controllerCheck } from './controllerRegister';
-
 window.$.leal = {
   ctrl: {},
   currentPage: '',
@@ -26,28 +21,8 @@ $.leal.view = (name, done, local) => {
   }).done(done);
 };
 
-const clearPath = (path) => {
-  const reg = /\w/
-  const internalPath = path;
-
-  if (!reg.test(internalPath[1])) {
-    internalPath = internalPath.slice(1)
-  }
-  if (!reg.test(internalPath[internalPath.length - 1])) {
-    internalPath = internalPath.slice(-1)
-  }
-
-  return internalPath;
-}
-
-export const initRoutes = (controllersPath, fallbackRoute) => {
-  if (!controllersPath) {
-    throw new Error('[initRoutes] => Informe o caminho da pasta de controllers.');
-  }
-
-  $.leal.controllersPath = clearPath(controllersPath);
-  $.leal.fallbackRoute = fallbackRoute;
-
-  $(window).on('hashchange', redirect);
-  redirect();
-}
+export { default as redirect } from './redirect';
+export { default as argument } from './argument';
+export { default as Controller } from './Controller';
+export { default as initRoutes } from './initRoutes';
+export { default as controllerRegister, controllerCheck } from './controllerRegister';
---
+++ src/initRoutes.js
@@ -0,0 +1,27 @@
+import redirect from './redirect';
+
+const clearPath = (path) => {
+  const reg = /\w/
+  const internalPath = path;
+
+  if (!reg.test(internalPath[1])) {
+    internalPath = internalPath.slice(1)
+  }
+  if (!reg.test(internalPath[internalPath.length - 1])) {
+    internalPath = internalPath.slice(-1)
+  }
+
+  return internalPath;
+}
+
+export default function initRoutes(controllersPath, fallbackRoute) {
+  if (!controllersPath) {
+    throw new Error('[initRoutes] => Informe o caminho da pasta de controllers.');
+  }
+
+  $.leal.controllersPath = clearPath(controllersPath);
+  $.leal.fallbackRoute = fallbackRoute;
+
+  $(window).on('hashchange', redirect);
+  redirect();
+}
